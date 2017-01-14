44050a10d0e721cd2dde03dcbe9f6c99c3ca382a
culled <coastersandthrills@gmail.com>
2017-01-14 00:36:54 -0800

    Added blank robot project

---
+++ 5468_2017_Code/src/org/usfirst/frc/team5468/robot/Robot.java
@@ -0,0 +1,81 @@
+
+package org.usfirst.frc.team5468.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot 
+{
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() 
+	{
+
+	}
+
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	@Override
+	public void disabledInit() 
+	{
+
+	}
+
+	@Override
+	public void disabledPeriodic() 
+	{
+		
+	}
+
+	@Override
+	public void autonomousInit() 
+	{
+
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() 
+	{
+		
+	}
+
+	@Override
+	public void teleopInit() 
+	{
+
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() 
+	{
+
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() 
+	{
+
+	}
+}
