9dbaca68e90665bb081367221a61d322d89e263d
culled <coastersandthrills@gmail.com>
2017-01-24 19:19:59 -0800

    Cleaned Up Vision Code

--- 5468_2017_Code/src/Plugins/Vision.java
+++ 5468_2017_Code/src/Plugins/Vision.java
@@ -6,7 +6,6 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.RotatedRect;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.imgproc.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -108,11 +107,21 @@ public class Vision
 		camera.setWhiteBalanceManual(whiteBalance);
 	}
 	
+	//This function returns the area of the current largest rectangle being tracked
 	public double getRectangleArea()
 	{
-		if(visionThread != null)
-			return visionThread.rect.size.area();
+		if(visionThread != null && visionThread.targetsFound > 0)
+			return visionThread.target1.size.area();
 		
+		return 0;
+	}
+	
+	//This function returns the aspect ratio of the current largest rectangle being tracked
+	public double getRectangleAspect()
+	{
+		if(visionThread != null && visionThread.targetsFound > 0)
+			return visionThread.target1.size.width / visionThread.target1.size.height;
+			
 		return 0;
 	}
 }
@@ -131,7 +140,6 @@ class VisionThread extends Thread
 	Mat outputImage;
 	Mat hsvImage;
 	Mat hierarchy;
-	Mat rectangles;
 	
 	//HSV mask bounds
 	Scalar lowerBounds;
@@ -142,11 +150,14 @@ class VisionThread extends Thread
 	//The time (in milliseconds) that the vision thread should wait
 	int threadWait;
 	
-	public volatile RotatedRect rect;
-	public volatile RotatedRect secondRect;
+	//These are variables returned by the vision system
+	//Since this class is being executed on a separate thread, they have to be volatile to make sure no conflicts arise when another script tries to access them
+	public volatile RotatedRect target1;
+	public volatile RotatedRect target2;
+	public volatile int targetsFound;
 	
 	//Called when an instance of this class is created
-	public VisionThread (String name, VideoSource videoSource, int width, int height, int wait)
+	public VisionThread (String name, VideoSource videoSource, int width, int height, double framerate)
 	{
 		threadName = name;
 		
@@ -161,13 +172,14 @@ class VisionThread extends Thread
 		outputImage = new Mat();
 		hsvImage = new Mat();
 		hierarchy = new Mat();
-		rectangles = new Mat();
 		
-		rect = new RotatedRect();
-		secondRect = new RotatedRect();
+		//Initialize our target variables
+		target1 = new RotatedRect();
+		target2 = new RotatedRect();
+		targetsFound = 0;
 		
-		//Calculate how long the thread should wait given a frequency
-		threadWait = wait;
+		//Calculate how long the thread should wait given a frequency (and ensure it waits at least 1 ms)
+		threadWait = Math.max((int)(Math.round(1.0 / framerate)) * 1000, 1);
 		
 		//Intialize the HSV mask bounds to default values
 		lowerBounds = new Scalar(0,0,0);
@@ -192,6 +204,8 @@ class VisionThread extends Thread
 			//Loop until this thread is interrupted
 			while(!Thread.interrupted())
 			{
+				long startTime = System.nanoTime();
+				
 				//Get the most recent image from the camera and store it
 				videoIn.grabFrame(inputImage);
 				
@@ -201,26 +215,21 @@ class VisionThread extends Thread
 				//Process the target
 				processTarget();
 				
-				if(rect != null)
-				{
-					Imgproc.ellipse(inputImage, rect, new Scalar(255, 0, 0), 3);
-					Imgproc.ellipse(inputImage, secondRect, new Scalar(0, 0, 255), 3);
-				}
-				//Put the processed image into the server so that the smartdashboard can view it
-				videoOut.putFrame(inputImage);
+				//Draws ellipses over the two tracked targets for debugging
+				visionDebug();
+				
+				//Calculate how much time has passed since we began analyzing this frame
+				long elapsedTime = (System.nanoTime() - startTime) / 1000;
 				
 				//Have this thread wait for some time to achieve a certain framerate
-				Thread.sleep(1);
+				Thread.sleep(Math.min(threadWait - elapsedTime, 1));
 			}
 			
 		}
-		catch(InterruptedException e)
-		{
-			
-		}
+		catch(InterruptedException e) {}
 	}
 	
-	//This function takes whatever image is stored in "inputImage", processes it, and saves the output in "outputImage"
+	//This function takes whatever image is stored in "inputImage" and processes it to find contours
 	void processImage()
 	{
 		//Vision procession code goes here
@@ -230,36 +239,99 @@ class VisionThread extends Thread
 		//Apply a color mask to the HSV image - and colors within the range are turned white, and the rest, black
 		Core.inRange(hsvImage, lowerBounds, upperBounds, outputImage);
 		
+		//Clear the list of contours
 		contours.clear();
 		
+		//Find all image contours and store them in the contours list
 		Imgproc.findContours(outputImage, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
-		
-		if (contours.size() > 0)
-		{
-			int biggestAreaIndex = 0;
-			int secondBiggestAreaIndex = 0;
-			double largestArea = 0;
-			
-			//Find the two largest contours and store them into their respective indexes
-			for(int i = 0; i < contours.size(); i++)
-			{
-				RotatedRect tempRect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
-				if(tempRect.size.area() > largestArea)
-				{
-					secondBiggestAreaIndex = biggestAreaIndex;
-					biggestAreaIndex = i;
-					largestArea = tempRect.size.area();
-				}
-			}
-			
-			rect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(biggestAreaIndex).toArray()));
-			secondRect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(secondBiggestAreaIndex).toArray()));
-		}
 	}
 	
 	//This function processes the HSV mask and works out target variables
 	void processTarget()
 	{
-		//TODO: Write target processing code
+		//Reset the number of found targets
+		targetsFound = 0;
+		
+		//If we have contours...
+		if (contours.size() > 0)
+		{
+			//Variables to cache values during the sorting process
+			int biggestAreaIndex = -1;
+			int secondBiggestAreaIndex = -1;
+			double largestArea = 0;
+			double secondLargestArea = 0;
+			RotatedRect currentRect = new RotatedRect();
+					
+			//Find the two largest rectangles and store them into their respective indices
+			for(int i = 0; i < contours.size(); i++)
+			{
+				//Make a rectangle that fits the current contours
+				currentRect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
+				
+				//Only process this rectangle if it is possibly a target
+				if(isPossibleTarget(currentRect))
+				{
+					//Store the current rectangle's area
+					double area = currentRect.size.area();
+					
+					//If this rectangle's area is greater than the current largest area...
+					if(area > largestArea)
+					{
+						//Save the previous largest rectangle as the second largest
+						secondBiggestAreaIndex = biggestAreaIndex;
+						secondLargestArea = largestArea;
+						
+						//Save the current rectangle as the largest
+						biggestAreaIndex = i;
+						largestArea = area;
+					}//If this area isn't the largest, but is largest than the current second largest...
+					else if(area > secondLargestArea)
+					{
+						//Save this rectangle as the second largest
+						secondLargestArea = area;
+						secondBiggestAreaIndex = i;
+					}
+				}
+			}
+			
+			//Store the largest rectangle
+			if(biggestAreaIndex >= 0)
+			{
+				target1 = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(biggestAreaIndex).toArray()));
+				targetsFound++;
+			}
+			
+			//Store the second largest rectangle
+			if(secondBiggestAreaIndex >= 0)
+			{
+				target2 = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(secondBiggestAreaIndex).toArray()));
+				targetsFound++;
+			}
+		}
+	}
+	
+	//This function draws ellipses over the tracked targets
+	void visionDebug()
+	{
+		if(target1 != null)
+		{
+			Imgproc.ellipse(inputImage, target1, new Scalar(255, 0, 255), 3);
+		}
+		
+		if(target2 != null)
+		{
+			Imgproc.ellipse(inputImage, target2, new Scalar(0, 0, 255), 3);
+		}
+		
+		//Put the processed image into the server so that the smartdashboard can view it
+		videoOut.putFrame(inputImage);
+	}
+	
+	//This function returns whether a rectangle could potentially be a target
+	boolean isPossibleTarget(RotatedRect rect)
+	{
+		//TODO: Write code to filter out targets from possible noise
+		
+		return true;
 	}
 }
