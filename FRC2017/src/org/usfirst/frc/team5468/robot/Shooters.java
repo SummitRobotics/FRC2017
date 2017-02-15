217fe91a7ca7cf0524833d6cbc4de91ffbee6e18
colin-ambrose-1617 <colinambrose123@gmail.com>
2017-02-14 18:10:06 -0800

    Shooters, Intake, and Gears-ish
    The shooter code is in it's own class, as Cullen suggested. We didn't know where to put the class, however, and it's probably in the wrong spot in terms of organization. Anyway, we tested it and it seemed to be working (Judging from the flashy lights on Talons). We also changed the button for toggling the solenoid we'll use for the gear pistons and added some basic code for activating the intake roller.
    - C^2

---
+++ FRC2017/src/org/usfirst/frc/team5468/robot/Shooters.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team5468.robot;
+
+public class Shooters{
+	
+	Robot mRobot;
+	
+	public Shooters(Robot mainRobot) {
+		mRobot = mainRobot;
+	}
+	
+	double shootStart = 0;
+	final double SHOOTER_DELAY = 1.5;
+	boolean shootersEnabled = false;
+	
+	public void shooterControl(boolean shooterButton)
+	{
+		if (shooterButton)
+		{
+			if(!shootersEnabled)
+			{
+				shootersEnabled = true;
+				
+				shootStart = System.currentTimeMillis();
+			}
+		} else
+		{
+			shootersEnabled = false;
+		}
+		
+		if(shootersEnabled)
+		{
+			mRobot.hardwareMap.rShooter.set(1);
+			mRobot.hardwareMap.lShooter.set(-1);
+			
+			if(System.currentTimeMillis() - shootStart > SHOOTER_DELAY * 1000)
+			{
+				mRobot.hardwareMap.rLoader.set(1);
+				mRobot.hardwareMap.lLoader.set(-1);
+			} else
+			{
+				mRobot.hardwareMap.rLoader.set(0);
+				mRobot.hardwareMap.lLoader.set(0);
+			}
+		} else
+		{
+			mRobot.hardwareMap.rShooter.set(0);
+			mRobot.hardwareMap.lShooter.set(0);
+			mRobot.hardwareMap.rLoader.set(0);
+			mRobot.hardwareMap.lLoader.set(0);
+		}
+	}
+}
